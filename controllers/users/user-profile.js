882660f9b186b9129957271fc4e26de9c23a41a2
santi <santimezquita@hotmail.es>
2022-02-24 23:37:02 +0100

    development mode set, helper ckeckData impelemented and a get messages funcion

--- controllers/users/user-profile.js
+++ controllers/users/user-profile.js
@@ -2,6 +2,7 @@ const User = require('../../managers/user2Manager.js')
 
 async function getUserProfile2(req, res){
     let result = await User.getUserProfile(req.params.userid);
+    console.log(req.params.userid);
     res.status(200).json(result);
 }
 
--- managers/messageManager.js
+++ managers/messageManager.js
@@ -1,2 +1,36 @@
-const Message = require('../models/messages');
+const Message = require('../models/messages.js');
 const myClient = require('./connection.js');
+const checkData = require('../helpers/checkData');
+
+class messageManager extends Message{
+
+    static async getMessagesWithGroup(groupid) {
+        myClient.connect();   
+        let data;
+        console.log('antes del try')
+        try {
+            data = await myClient.query(`SELECT * FROM messages where from_group = ${groupid} or to_group = ${groupid};`)
+        } 
+        catch(err) {
+            console.log('ERROR!!!!!');
+        }
+        finally {
+            myClient.end(); 
+        }
+        return checkData(data);
+    }
+/*     static async getUserProfile(id) {
+        myClient.connect();   
+        let data;
+        try {
+            data = await myClient.query(`SELECT * FROM users where user_id = ${id};`)
+        } 
+        catch(err) {
+            console.log('ERROR!!!!!');
+        };
+        myClient.end(); 
+        return data.rows[0];        
+    } */
+    
+  }
+module.exports = messageManager;
