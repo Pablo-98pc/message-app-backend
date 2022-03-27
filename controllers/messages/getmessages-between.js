ef1d7c46e68f4c5dd74356d01009b387fbf0fb72
aldo <aldofcastellanop@gmail.com>
2022-03-27 19:03:25 +0200

    change manager/messges/get message for user

--- controllers/messages/getmessages-between.js
+++ controllers/messages/getmessages-between.js
@@ -1,8 +1,11 @@
-const Message = require('../../managers/messageManager.js')
+const Message = require("../../managers/messageManager.js");
 
-async function getMessagesBetween(req, res){
-    let result = await Message.getMessagesBetween(req.params.user1, req.params.user2);
-        res.status(200).json(result);
+async function getMessagesBetween(req, res) {
+  let result = await Message.getMessagesBetween(
+    req.params.user1,
+    req.params.user2,
+  );
+  res.status(200).json(result);
 }
 
 module.exports = getMessagesBetween;
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-require('dotenv').config(); 
-var md5 =require('md5');
-var app = require('express')();
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const cors = require("cors");
+var md5 = require("md5");
+var app = require("express")();
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(cors());
 /* app.use(require('./middlewares/checkBody')); */
-app.use(process.env.ROOT, require('./routes'));
+app.use(process.env.ROOT, require("./routes"));
 app.listen(3001, () => {
-console.log('Working!!!')
+  console.log("Working!!!");
 });
